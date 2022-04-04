ae88bb06345a24067dd6354eeb8212ab7e3ddc81
Astavie <astavie@protonmail.com>
2022-04-04 20:48:28 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         let lastVersion = new Date().getTime();
 
-        const full = "https://raw.githubusercontent.com/Astavie/anarchychessoverlay/6891398f8823efb98b72f1c8585f1cb44abe61ff/overlay.png";
+        const full = "https://raw.githubusercontent.com/Astavie/anarchychessoverlay/main/overlay.png";
 
         //region image manipulation
         const getImageData = async () => {
